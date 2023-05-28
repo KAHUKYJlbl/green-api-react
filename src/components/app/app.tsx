1187840f699e4f1ea79c043b249cc631d5210dc7
Iurii Pototskii <iuriipototskii2gmail.com>
2023-05-28 21:42:49 +0200

    feat: main markup

---
+++ src/components/app/app.tsx
@@ -0,0 +1,302 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="page-wrapper">
+      <header className="header">
+        <div className="user">
+          <svg width="49" height="49" aria-hidden="true">
+            <image href="./images/icons/user.svg" />
+          </svg>
+
+          <p className="user-name">
+            GREEN-API-USER
+          </p>
+
+          <svg width="49" height="49" aria-hidden="true">
+            <image href="./images/icons/logout.svg" />
+          </svg>
+        </div>
+      </header>
+
+      <div className="contacts">
+        <div className="new-contact"></div>
+
+        <ul className="contacts-list">
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true" className="x-button">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+
+          <li className="contacts-list_item contacts-list_item__current">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+          <li className="contacts-list_item">
+            <div className="user">
+              <svg width="49" height="49" aria-hidden="true">
+                <image href="./images/icons/user.svg" />
+              </svg>
+
+              <p className="user-name">
+                +7 (812) 999-11-11
+              </p>
+
+              <svg width="24" height="24" aria-hidden="true">
+                <image href="./images/icons/x-button.svg" />
+              </svg>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div className="chat">
+        <ul className="chat-list">
+          <li className="chat-list_item">
+
+          </li>
+          <li className="chat-list_item chat-list_item__received">
+            
+          </li>
+          <li className="chat-list_item">
+            
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
